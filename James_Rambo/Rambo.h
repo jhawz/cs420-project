614257a4fac85fa6be824992efa464e07d1bedc3
freegyp <gyp.08@live.cn>
2014-10-15 17:02:49 -0400

    Create Rambo.h

---
+++ James_Rambo/Rambo.h
@@ -0,0 +1,30 @@
+#ifndef __Rambo__
+#define __Rambo__
+
+#include <cstdio>
+#include <iostream>
+#include <sfml/Graphics.hpp>
+#include "Animation.h"
+
+class Rambo:public sf::Sprite{
+public:
+    Rambo();
+    ~Rambo();
+    void prepareFrameInfo();
+    void update();
+    void rightRun();
+    void leftRun();
+    void standStill();
+    void jump();
+    
+private:
+    bool rightdir;
+    bool running;
+    float vx,vy,vxmax,vymax,vxmin,vymin;
+    float ax,ay;
+    Animation *curAnim;
+    Animation *run;
+    Animation *stand;
+};
+
+#endif /* defined(__Rambo__) */
