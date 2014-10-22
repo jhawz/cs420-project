cfc013184912cda1b873c4e2e548d829539b4d43
freegyp <gyp.08@live.cn>
2014-10-22 18:44:01 -0400

    Update Rambo.h

--- include/Rambo.h
+++ include/Rambo.h
@@ -3,31 +3,55 @@
 
 #include <cstdio>
 #include <iostream>
-#include <SFML/Graphics.hpp>
+#include <sfml/Graphics.hpp>
+#include <sfml/System/Time.hpp>
 #include "Animation.h"
 
-class Rambo : public sf::Sprite {
+class Rambo:public sf::Sprite{
 public:
     Rambo();
     ~Rambo();
-    void prepareRamboFrameInfo();
-    void prepareBondFrameInfo();
+    void setOriginalImg(sf::Image &image);
+    void prepareFrameInfo();
     void update();
     void rightRun();
     void leftRun();
     void standStill();
     void jump();
-
+    void shoot();
+    void upshoot();
+    void downshoot();
+    void crouchStill();
+    void crouchshoot();
+    void shotDead();
+    bool isAlive();
+    bool lowCollide(int ground);
+    
 private:
+    int getLowBound();
+    sf::Clock clock;
+    bool jumping;
+    bool alive;
     bool rightdir;
     bool running;
-    bool shortJumping;
-    float vx, vy, vxmax, vymax, vxmin, vymin;
-    float ax, ay;
+    bool shootlocked;
+    int lowerBound;
+    float vx,vy,vxmax,vymax,vxmin,vymin;
+    float ax,ay;
     Animation *curAnim;
     Animation *run;
     Animation *stand;
-    Animation *shortJump;
+    Animation *straightShoot;
+    Animation *upShoot;
+    Animation *downShoot;
+    Animation *jumpPrepare;
+    Animation *jumpUp;
+    Animation *jumpFloat;
+    Animation *jumpFall;
+    Animation *crouch;
+    Animation *crouchShoot;
+    Animation *die;
+    sf::Image *imgOrigin;
 };
 
 #endif /* defined(__Rambo__) */
