cfc7d0b002895c3ea2acf3d478b1be26892c0f2b
freegyp <gyp.08@live.cn>
2014-10-22 18:41:49 -0400

    Update Animation.cpp

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -1,58 +1,70 @@
 #include "Animation.h"
 
-Animation::Animation() {
-    repeatable = true;
-    curFrame = 0;
-    period = sf::milliseconds(100);
-    frameCount = 0;
+Animation::Animation(){
+    curFrame=0;
+    period=sf::milliseconds(100);
+    frameCount=0;
     clock.restart();
 }
 
-void Animation::setAnimationPeriod(int minisec) {
-    period = sf::milliseconds(minisec);
+void Animation::setAnimationPeriod(int minisec){
+    period=sf::milliseconds(minisec);
 }
 
-void Animation::addFrame(sf::IntRect frame) {
-    m_frames.push_back(frame);
+void Animation::addFrame(sf::IntRect frame){
+    sf::IntRect rect(frame);
+    if (rect.width<0) {
+        rect.left+=frame.width;
+        rect.width*=-1;
+    }
+    if (rect.height<0) {
+        rect.top+=frame.height;
+        rect.height*=-1;
+    }
+    m_frames.push_back(rect);
+    
     frameCount++;
 }
 
-sf::IntRect Animation::getCurFrame() {
+sf::IntRect Animation::getCurFrame(){
     return m_frames[curFrame];
 }
 
-bool Animation::play() {
-    int elapsedPeriods = clock.getElapsedTime().asMilliseconds() / period.asMilliseconds();
-    if ((!repeatable)&&(curFrame >= frameCount)) {
+bool Animation::play(){
+    int elapsedPeriods=clock.getElapsedTime().asMilliseconds()/period.asMilliseconds();
+    if ((!repeatable)&&(curFrame>=frameCount-1)) {
         end();
     }
-    if (!ended) {
+    if(!ended){
         switch (elapsedPeriods) {
             case 0:
                 return false;
             default:
-                curFrame += elapsedPeriods;
-                curFrame = curFrame % frameCount;
+                curFrame+=elapsedPeriods;
+                curFrame=curFrame%frameCount;
                 clock.restart();
                 return true;
                 break;
         }
-    } else return false;
+    }
+    else return false;
 }
 
-void Animation::setRepeatable(bool rep) {
-    repeatable = rep;
+void Animation::setRepeatable(bool rep){
+    repeatable=rep;
 }
 
-void Animation::restart() {
-    ended = false;
-    curFrame = 0;
+void Animation::restart(){
+    ended=false;
+    curFrame=0;
 }
 
-void Animation::end() {
-    ended = true;
+void Animation::end(){
+    curFrame=0;
+    ended=true;
 }
 
-bool Animation::isEnded() {
+bool Animation::isEnded(){
     return ended;
 }
+
