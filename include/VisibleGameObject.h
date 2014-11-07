607d2196924ae624dd4c727d1654e6918dc1f908
jch <jch@bigfoot>
2014-11-06 19:32:41 -0500

    Visible Game object header and source added.

---
+++ include/VisibleGameObject.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "stdafx.h"
+class VisibleGameObject
+{
+public:
+	VisibleGameObject();
+	virtual ~VisibleGameObject();
+	
+	virtual void Load(std::string filename);
+	virtual void Draw(sf::RenderWindow & window);
+	virtual void Update(float elapsedTime);
+
+	virtual void SetPosition(float x, float y);
+	virtual sf::Vector2f GetPosition() const;
+	virtual bool IsLoaded() const;
+
+protected:
+	sf::Sprite& GetSprite();
+
+private:
+	sf::Sprite  _sprite;
+	sf::Texture _image;
+	std::string _filename;
+	bool _isLoaded;
+	
+};
