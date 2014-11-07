0ce585a95b8c65b196ff62dd47e7aff68e8c3909
jch <jch@bigfoot>
2014-11-06 19:37:16 -0500

    stdafx.h and stdafx.cpp to allow common includes to be declared with one header file

---
+++ include/stdafx.h
@@ -0,0 +1,16 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+// TODO: reference additional headers your program requires here
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+#include <map>
+#include <iostream>
+#include <cassert>
