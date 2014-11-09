5d7f9a47e9758a71f33e48816dc59bed01d27bfb
freegyp <freegyp@gmail.com>
2014-11-09 12:32:13 -0500

    Update GameObjectManager.cpp

--- src/GameObjectManager.cpp
+++ src/GameObjectManager.cpp
@@ -45,7 +45,6 @@ void GameObjectManager::DrawAll(sf::RenderWindow& renderWindow) {
 void GameObjectManager::UpdateAll() {
     std::map<std::string, VisibleGameObject*>::const_iterator itr = gameObjects.begin();
     float timeDelta = clock.restart().asSeconds();
-    std::cout<<"before iteration"<<std::endl;
 
     while (itr != gameObjects.end()) {
         itr->second->Update(timeDelta);
