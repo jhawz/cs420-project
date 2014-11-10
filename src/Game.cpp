db1cd1aee90fe937ee2beabb35bb3c9d948c1462
Jagabel <jagabel@email.wm.edu>
2014-11-10 16:01:45 -0500

    Loads in tiles for the map; background and collisions to come. The load time has increased significantly (on my machine)...Need to look into this as a potential issue.

--- src/Game.cpp
+++ src/Game.cpp
@@ -5,6 +5,8 @@
 #include "Enemy.h"
 #include "Bond.h"
 #include "VisibleGameObject.h"
+#include "MapLoader.h"
+#include "Tile.h"
 
 void Game::Start(void)
 {
@@ -14,7 +16,22 @@ void Game::Start(void)
 	mainWindow.create(sf::VideoMode(SCREEN_WIDTH,SCREEN_HEIGHT,32),"James Rambo: Revenge of WillFinger!");
 	
 	//mainWindow.SetFramerateLimit(60);
-        
+               sf::Texture text;
+        MapLoader* m = new MapLoader();
+        m->loadMap("data/Level1.txt");
+        std::vector<std::string> testList = m->getTileCodes();
+        text.loadFromFile("textures/TiledEight32New.png");
+        for (int x = 0; x < testList.size(); x++)
+        {
+            Tile* newTile = new Tile();
+            newTile->buildTile(std::stoi(testList[x]), text, 
+                    sf::Vector2i(((std::stoi(testList[x]) - 1) % 15) * 32, 
+                    ((std::stoi(testList[x]) - 1) / 15) * 32),
+                sf::Vector2i(32, 32));
+            newTile->SetPosition((x % 100) * 32, (x / 100) * 32);
+            gameObjectManager.Add("Tile" + std::to_string(x), newTile);
+        }
+        delete m;
         Enemy *enemy_1 = new Enemy("actors.xml", "textures/enemy.png", "Pistol");
         enemy_1->SetPosition(400,400);
         
