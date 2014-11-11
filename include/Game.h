e55fb831a26a5c504d1219be200cd1dc7f76d5d8
Joe Hause <jhause@gmail.com>
2014-11-11 10:57:38 -0500

    A view has been added but screen scrolling is not yet complete.

--- include/Game.h
+++ include/Game.h
@@ -3,27 +3,30 @@
 #include "SFML/Graphics.hpp"
 #include "GameObjectManager.h"
 
-class Game
-{
+class Game {
 public:
-	static void Start();
-	static sf::RenderWindow& GetWindow();
-	const static sf::Event& GetInput();
-	const static int SCREEN_WIDTH = 800;
-	const static int SCREEN_HEIGHT = 600;
+    static void Start();
+    static sf::RenderWindow& GetWindow();
+    const static sf::Event& GetInput();
+    const static int SCREEN_WIDTH = 800;
+    const static int SCREEN_HEIGHT = 600;
 
 private:
-	static bool IsExiting();
-	static void GameLoop();
-	
-	static void ShowSplashScreen();
-	static void ShowMenu();
+    static bool IsExiting();
+    static void GameLoop();
 
-	enum GameState { Uninitialized, ShowingSplash, Paused, 
-					ShowingMenu, Playing, Exiting };
+    static void ShowSplashScreen();
+    static void ShowMenu();
 
-	static GameState gameState;
-	static sf::RenderWindow mainWindow;
-	
-	static GameObjectManager gameObjectManager;
+    enum GameState {
+        Uninitialized, ShowingSplash, Paused,
+        ShowingMenu, Playing, Exiting
+    };
+
+    static GameState gameState;
+    static sf::RenderWindow mainWindow;
+    static sf::View view;
+    static sf::Vector2f cameraPosition;
+
+    static GameObjectManager gameObjectManager;
 };
--- src/Game.cpp
+++ src/Game.cpp
@@ -8,140 +8,141 @@
 #include "MapLoader.h"
 #include "Tile.h"
 
-void Game::Start(void)
-{
-	if(gameState != Uninitialized)
-		return;
-	
-	mainWindow.create(sf::VideoMode(SCREEN_WIDTH,SCREEN_HEIGHT,32),"James Rambo: Revenge of WillFinger!");
-	
-	//mainWindow.SetFramerateLimit(60);
-                      sf::Texture text;
-        MapLoader* m = new MapLoader();
-        VisibleGameObject* bg = m->getBackground(1);
-        m->loadMap("data/Level1.txt");
-        std::vector<std::string> codeList = m->getTileCodes();
-        std::vector<int> collisionList;
-        text.loadFromFile("textures/TiledEight32New.png");
-        gameObjectManager.Add("Background", bg);
-        for (int x = 0; x < codeList.size(); x++)
-        {
-            if (codeList[x] != "0")
-            {
+void Game::Start(void) {
+    if (gameState != Uninitialized)
+        return;
+
+    mainWindow.create(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32), "James Rambo: Revenge of WillFinger!");
+
+    //mainWindow.SetFramerateLimit(60);
+    sf::Texture text;
+    MapLoader* m = new MapLoader();
+    VisibleGameObject* bg = m->getBackground(1);
+    m->loadMap("data/Level1.txt");
+    std::vector<std::string> codeList = m->getTileCodes();
+    std::vector<int> collisionList;
+    text.loadFromFile("textures/TiledEight32New.png");
+    gameObjectManager.Add("Background", bg);
+    for (int x = 0; x < codeList.size(); x++) {
+        if (codeList[x] != "0") {
             Tile* newTile = new Tile();
-            newTile->buildTile(std::stoi(codeList[x]), text, 
-                    sf::Vector2i(((std::stoi(codeList[x]) - 1) % 15) * 32, 
+            newTile->buildTile(std::stoi(codeList[x]), text,
+                    sf::Vector2i(((std::stoi(codeList[x]) - 1) % 15) * 32,
                     ((std::stoi(codeList[x]) - 1) / 15) * 32),
-                sf::Vector2i(32, 32));
+                    sf::Vector2i(32, 32));
             newTile->SetPosition((x % 100) * 32, (x / 100) * 32);
             collisionList.push_back(x % 100 + ((x / 100) * 100));
             gameObjectManager.Add("Tile" + std::to_string(x), newTile);
-            }
         }
-        gameObjectManager.setCollisionList(collisionList);
-        delete m;
-        
-        Enemy *enemy_1 = new Enemy("actors.xml", "textures/enemy.png", "Pistol");
-        enemy_1->SetPosition(300 ,200);
-        enemy_1->setBoundary(0, 0, 1024, 464);
-        
-        Bond *bond = new Bond("actors.xml", "textures/JB.png");
-        bond->SetPosition(200,200);
-        bond->setBoundary(0, 0, 1024, 464);
-        
-        gameObjectManager.Add("Bond", bond);
-        gameObjectManager.Add("Enemy", enemy_1);
-        
-	gameState= Game::ShowingSplash;
+    }
+    gameObjectManager.setCollisionList(collisionList);
+    delete m;
 
-	while(!IsExiting())
-	{
-		GameLoop();
-	}
+    Enemy *enemy_1 = new Enemy("actors.xml", "textures/enemy.png", "Pistol");
+    enemy_1->SetPosition(300, 200);
+    enemy_1->setBoundary(0, 0, 800, 464);
 
-	mainWindow.close();
+    Bond *bond = new Bond("actors.xml", "textures/JB.png");
+    bond->SetPosition(200, 200);
+    bond->setBoundary(0, 0, 800, 464);
+
+    gameObjectManager.Add("Bond", bond);
+    gameObjectManager.Add("Enemy", enemy_1);
+
+    view.reset(sf::FloatRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
+    view.setViewport(sf::FloatRect(0, 0, 1.0f, 1.0f));
+
+    gameState = Game::ShowingSplash;
+
+    while (!IsExiting()) {
+        GameLoop();
+    }
+
+    mainWindow.close();
 }
 
-bool Game::IsExiting()
-{
-	if(gameState == Game::Exiting) 
-		return true;
-	else 
-		return false;
+bool Game::IsExiting() {
+    if (gameState == Game::Exiting)
+        return true;
+    else
+        return false;
 }
 
-
-sf::RenderWindow& Game::GetWindow()
-{
-	return mainWindow;
+sf::RenderWindow& Game::GetWindow() {
+    return mainWindow;
 }
 
-const sf::Event& Game::GetInput() 
-{
-	sf::Event currentEvent;
-	mainWindow.pollEvent(currentEvent);
-	return currentEvent;
+const sf::Event& Game::GetInput() {
+    sf::Event currentEvent;
+    mainWindow.pollEvent(currentEvent);
+    return currentEvent;
 }
 
-void Game::GameLoop()
-{
-	sf::Event currentEvent;
-	mainWindow.pollEvent(currentEvent);
-	
-	switch(gameState)
-	{
-		case Game::ShowingMenu:
-			{
-				ShowMenu();
-				break;
-			}
-		case Game::ShowingSplash:
-			{
-				ShowSplashScreen();
-				break;
-			}
-		case Game::Playing:
-			{
-				mainWindow.clear(sf::Color::White);
+void Game::GameLoop() {
+    sf::Event currentEvent;
+    mainWindow.pollEvent(currentEvent);
 
-				gameObjectManager.UpdateAll();
-				gameObjectManager.DrawAll(mainWindow);
-                                
-				mainWindow.display();
-				if(currentEvent.type == sf::Event::Closed) gameState = Game::Exiting;
+    switch (gameState) {
+        case Game::ShowingMenu:
+        {
+            ShowMenu();
+            break;
+        }
+        case Game::ShowingSplash:
+        {
+            ShowSplashScreen();
+            break;
+        }
+        case Game::Playing:
+        {
 
-				if(currentEvent.type == sf::Event::KeyPressed)
-					{
-						if(currentEvent.key.code == sf::Keyboard::Escape) ShowMenu();
-					}
+            //cameraPosition.x = bond->GetPosition().x + 32 - (SCREEN_WIDTH / 2);
+            //cameraPosition.y = bond->GetPosition().y + 32 - (SCREEN_HEIGHT / 2);
 
-				break;
-			}
-	}
+            //if (cameraPosition.x < 0)
+            //    cameraPosition.x = 0;
+            //if (cameraPosition.y < 0)
+            //    cameraPosition.y = 0;
+
+            mainWindow.clear(sf::Color::White);
+            mainWindow.setView(view);
+
+            gameObjectManager.UpdateAll();
+            gameObjectManager.DrawAll(mainWindow);
+
+            mainWindow.display();
+            if (currentEvent.type == sf::Event::Closed) gameState = Game::Exiting;
+
+            if (currentEvent.type == sf::Event::KeyPressed) {
+                if (currentEvent.key.code == sf::Keyboard::Escape) ShowMenu();
+            }
+
+            break;
+        }
+    }
 }
 
-void Game::ShowSplashScreen()
-{
-	SplashScreen splashScreen;
-	splashScreen.Show(mainWindow);
-	gameState = Game::ShowingMenu;
+void Game::ShowSplashScreen() {
+    SplashScreen splashScreen;
+    splashScreen.Show(mainWindow);
+    gameState = Game::ShowingMenu;
 }
 
-void Game::ShowMenu()
-{
-	MainMenu mainMenu;
-	MainMenu::MenuResult result = mainMenu.Show(mainWindow);
-	switch(result)
-	{
-		case MainMenu::Exit:
-			gameState = Exiting;
-			break;
-		case MainMenu::Play:
-			gameState = Playing;
-			break;
-	}
+void Game::ShowMenu() {
+    MainMenu mainMenu;
+    MainMenu::MenuResult result = mainMenu.Show(mainWindow);
+    switch (result) {
+        case MainMenu::Exit:
+            gameState = Exiting;
+            break;
+        case MainMenu::Play:
+            gameState = Playing;
+            break;
+    }
 }
 
 Game::GameState Game::gameState = Uninitialized;
 sf::RenderWindow Game::mainWindow;
+sf::View Game::view;
+sf::Vector2f Game::cameraPosition;
 GameObjectManager Game::gameObjectManager;
