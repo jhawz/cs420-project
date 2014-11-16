c40b58d6611aa17069b1ad5a5210ebb283db424f
freegyp <freegyp@gmail.com>
2014-11-16 17:45:12 -0500

    Create Powerup.h

---
+++ include/Powerup.h
@@ -0,0 +1,23 @@
+#ifndef POWERUP_H
+#define POWERUP_H
+
+
+#include <stdio.h>
+#include "stdafx.h"
+#include "VisibleGameObject.h"
+#include "Animation.h"
+
+class Powerup:public VisibleGameObject{
+public:
+    Powerup(std::string config,std::string texture);
+    void Update(float elapsedTime);
+    bool isDead();
+private:
+    bool used;
+    void prepareFrameInfo(pugi::xml_node& node);
+    void animReq(std::string animName);
+    std::map<std::string,Animation*> animations;
+    Animation *curAnim;
+};
+
+#endif
