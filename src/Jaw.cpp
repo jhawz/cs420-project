1d08d829557cfbfd0b63fff269ca3b99feb38f14
freegyp <freegyp@gmail.com>
2014-11-16 17:46:17 -0500

    Create Jaw.cpp

---
+++ src/Jaw.cpp
@@ -0,0 +1,31 @@
+#include "Jaw.h"
+
+Jaw::Jaw(std::string config, std::string texture):Actor::Actor(){
+    objectType="Jaw";
+    pugi::xml_document doc;
+    doc.load_file(config.c_str());
+    
+    pugi::xml_node jawnode = doc.child("root").find_child_by_attribute("Actor", "name", "Jaw");
+    
+    if (jawnode.empty()) {
+        //if there is an error in node initialization
+        std::cout << "Can't find Bond actor node..." << std::endl;
+        return;
+    }
+    //  std::cout << "start preparing frames" << std::endl;
+    prepareFrameInfo(jawnode);
+    // std::cout << "Finish preparing frames" << std::endl;
+    Load(texture);
+    sf::Image *img = new sf::Image();
+    if (!img->loadFromFile(texture)) {
+        return;
+    }
+    setOriginalImg(*img);
+    type = 3;
+}
+
+void Jaw::attack(Actor& actor){
+    if (actor.type==1&&closeContact(actor)) {
+        Actor::attack();
+    }
+}
